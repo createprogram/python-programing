9dc095f3f05ee422ae72aa2f98a21585e84d24f4
createprogram <108576270+createprogram@users.noreply.github.com>
2022-07-18 11:46:36 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,28 @@
+# loops in python
+#1.while loop
+#ex1
+
+i=1
+while i<=5: 
+    print(i)
+    i=i+1
+
+#ex2
+i=1
+while i<=5: 
+    print(i*"*")
+    i=i+1
+
+#ex3
+i=5
+while i>=1: 
+    print(i*"*")
+    i=i-1
+
+
+#2.for loop
+ for i in range(5):
+    print(i)
+
+
+
